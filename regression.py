df5d04bb964a29c91cb5912adc7322e155a43f20
Magecatz <Magecatz@gmail.com>
2020-03-30 04:20:14 -0400

    Test v2

--- regression.py
+++ regression.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import quandl
 import math
+import numpy as np
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
 
 quandl.ApiConfig.api_key = 'xEgXfFpmyJ9usGFxzJNw'
 
@@ -20,7 +23,23 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
 forecast_out = int(math.ceil(0.01*len(df)))
+print(forecast_out, 365*0.01)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
-print(df.head())
+x = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+
+x = preprocessing.scale(x)
+
+x = x[:-forecast_out]
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(x_train, y_train)
+accuracy = clf.score(x_test, y_test)
+
+print(accuracy)
