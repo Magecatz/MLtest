7cd90aada0158e52f0672c4985ff97ced690293b
Magecatz <Magecatz@gmail.com>
2020-03-31 21:02:52 -0400

    Test v3 bestfit

--- venv/p2/classification.py
+++ venv/p2/classification.py
@@ -1,5 +1,22 @@
 from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import preprocessing, model_selection, neighbors
+import pandas as pd
 
+df = pd.read_csv('breast-cancer-wisconsin.data')
+#print(df.columns)
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
 
+x = np.array(df.drop(['class'], 1))
+y = np.array(df['class'])
+
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x,y,test_size=0.2)
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(x_train, y_train)
+
+accuracy = clf.score(x_test, y_test)
+
+print(accuracy)
